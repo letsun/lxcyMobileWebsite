bba378180076187661929edc69b78bbf2e2fb98a
ENOKEEIRYIBV6MZ\Administrator <653353427@qq.com>
2019-05-16 11:30:41 +0800

    111

--- scripts/lib/config.js
+++ scripts/lib/config.js
@@ -1,11 +1,4 @@
 var ip = '';
 var config = {
-    "getVideos":ip + '/wap/basedata/getVideos',    // 获取视频列表
-    "videoEva":ip + '/wap/basedata/videoEva',    // 视频评论
-    "getVideoEvas":ip + '/wap/basedata/getVideoEvas',    // 获取视频评论列表
-    "getArticles":ip + '/wap/basedata/getArticles',    // 获取文章列表
-    "articleEva":ip + '/wap/basedata/articleEva',    // 文章评论
-    "getArticleEvas":ip + '/wap/basedata/getArticleEvas',    // 获取文章评论列表
-    "articleDetail":ip + '/wap/basedata/articleDetail',    // 获取文章详情
-    "praise":ip + '/wap/basedata/praise'    // 点赞
+
 };
